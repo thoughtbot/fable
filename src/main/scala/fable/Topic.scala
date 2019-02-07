582495aa635d323e5d42811f909e5833cae99802
Joe Ferris <jferris@thoughtbot.com>
2019-02-07 11:49:20 -0500

    Add PureConfig reader for Topic
    Clients will need to pull these from configuration

--- src/main/scala/fable/Topic.scala
+++ src/main/scala/fable/Topic.scala
@@ -1,10 +1,15 @@
 package fable
 
+import pureconfig.ConfigReader
+import pureconfig.generic.semiauto.deriveReader
+
 /**
   * Value class for topic names.
   */
 case class Topic private (name: String) extends AnyVal
 
-private[fable] object Topic {
+object Topic {
   private[fable] def apply(name: String): Topic = new Topic(name)
+
+  implicit val topicConfigReader: ConfigReader[Topic] = deriveReader
 }
