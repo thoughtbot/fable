1491cd13ef848b55acf3b118d571e1cdf25167d5
Joe Ferris <jferris@thoughtbot.com>
2019-03-29 15:49:13 -0400

    Value classes can be public
    These were originally private to prevent prefixes from leaking. We've
    switched to handling prefixes in application config, so these can be
    publicly accessible now.

--- src/main/scala/fable/GroupId.scala
+++ src/main/scala/fable/GroupId.scala
@@ -6,7 +6,7 @@ import pureconfig.generic.semiauto.deriveReader
 /**
   * Value class for consumer group IDs.
   */
-case class GroupId private (name: String) extends AnyVal
+case class GroupId(name: String) extends AnyVal
 
 object GroupId {
   implicit val groupIdConfigReader: ConfigReader[GroupId] = deriveReader
--- src/main/scala/fable/Topic.scala
+++ src/main/scala/fable/Topic.scala
@@ -6,10 +6,8 @@ import pureconfig.generic.semiauto.deriveReader
 /**
   * Value class for topic names.
   */
-case class Topic private (name: String) extends AnyVal
+case class Topic(name: String) extends AnyVal
 
 object Topic {
-  private[fable] def apply(name: String): Topic = new Topic(name)
-
   implicit val topicConfigReader: ConfigReader[Topic] = deriveReader
 }
